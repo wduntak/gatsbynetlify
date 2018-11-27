a6d156801396e0c4ec5be900102567c6f5e32c5a
wduntak <wduntak@gmail.com>
2018-11-26 23:03:48 -0500

    Add netlify cms package

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
+    <img src="https://images.unsplash.com/photo-1542754579-1cc220ca016b?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&h=200&fit=crop&ixid=eyJhcHBfaWQiOjF9&s=99b2b86bba5f07bf03f3a05b877c8bc2" alt="" />    
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
